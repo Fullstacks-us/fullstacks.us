9c452683bc96fa67eafb0687a6f310ed5be1877b
Fullstacks-us <inbox@fullstacks.us>
2025-07-26 01:01:40 -0400

    Move map section below contact form
    Reordered the Contact component layout so that the map iframe appears after the contact form instead of before it. This improves the visual flow of the contact page.

--- fullstacks-us/src/components/Contact.tsx
+++ fullstacks-us/src/components/Contact.tsx
@@ -18,17 +18,6 @@ export default function Contact() {
         <h1 className="heading">Get in touch!</h1>
       </section>
       <div className="contact-in">
-        <div className="contact-map">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d193595.25436351647!2d-75!3d42!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1644684739958!5m2!1sen!2sus" 
-            width="100%" 
-            height="450" 
-            style={{ border: 0 }} 
-            allowFullScreen 
-            loading="lazy"
-          ></iframe>
-        </div>
-        
         <div className="contact-form">
           <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Name" className="contact-form-txt" required />
@@ -39,6 +28,17 @@ export default function Contact() {
               {isSubmitted ? 'Message Sent!' : 'Submit'}
             </button>
           </form>
+        </div>
+        
+        <div className="contact-map">
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d193595.25436351647!2d-75!3d42!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1644684739958!5m2!1sen!2sus" 
+            width="100%" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowFullScreen 
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
     </>
