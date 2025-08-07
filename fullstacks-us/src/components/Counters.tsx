d7b1e79c5970bbfac49491b61e9813e4602be5a6
steeltiger1116 <steeltiger1116@outlook.com>
2025-08-07 16:44:27 +0900

    update counters

--- fullstacks-us/src/components/Counters.tsx
+++ fullstacks-us/src/components/Counters.tsx
@@ -4,22 +4,22 @@ export default function Counters() {
       <div className="container">
         <div>
           <i className="far fa-clock fa-4x"></i>
-          <div className="counter" data-target="13500">0</div>
+          <div className="counter" data-target="13500">128</div>
           <h3>Working Hours</h3>
         </div>
         <div>
           <i className="fas fa-gift fa-4x"></i>
-          <div className="counter" data-target="720">0</div>
+          <div className="counter" data-target="720">17</div>
           <h3>Completed Projects</h3>
         </div>
         <div>
           <i className="fas fa-users fa-4x"></i>
-          <div className="counter" data-target="480">0</div>
+          <div className="counter" data-target="480">15</div>
           <h3>Happy Clients</h3>
         </div>
         <div>
           <i className="fas fa-award fa-4x"></i>
-          <div className="counter" data-target="120">0</div>
+          <div className="counter" data-target="120">37</div>
           <h3>Awards Received</h3>
         </div>
       </div>
